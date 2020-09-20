c424d3bd7a18e3b59c8d82293a2368f283cd25cf
Mark Tellez <mark.tellez@icloud.com>
2020-09-20 01:09:01 -0500

    added text field

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,25 +1,5 @@
-import React, { useState } from 'react';
-import './styles.css';
+import React from 'react';
 
 export default function App() {
-  const [value, setValue] = useState('');
-  const [focused, setFocused] = useState(false);
-
-  return (
-    <div
-      className={`form-field ${focused ? 'is-focused' : ''} ${
-        value.length > 0 ? 'has-value' : ''
-      }`}>
-      <div className='control'>
-        <label>Label</label>
-        <input
-          type='text'
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          onFocus={() => setFocused(true)}
-          onBlur={() => setFocused(false)}
-        />
-      </div>
-    </div>
-  );
+  return <div>start here</div>;
 }
